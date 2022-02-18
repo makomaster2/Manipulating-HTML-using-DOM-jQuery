f7e0eb4acd49f615cc04f3ae95c89f4ff1b8b3c1
Austin Jones <makomaster2@gmail.com>
2022-02-18 12:59:37 -0600

    formatted

--- DOM/app.js
+++ DOM/app.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
 
     // #1. Make a button appear on the page when it's first loaded. The button cannot already be in your html. When the button is clicked, you should display an Alert box with any (nice) message.
-
     let button = document.createElement('button');
     let btnText = document.createTextNode('Say something nice!');
     button.appendChild(btnText);
@@ -12,7 +11,6 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     // #2. Make a button and text box (you can just put them in your HTML). When the button is clicked, display an alert with the message that is typed in the text box.
-
     let alertButton = document.getElementById('alertButton');
 
     alertButton.addEventListener('click', function () {
@@ -21,7 +19,6 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     // #3. Create a div in HTML. Without using CSS :hover, make the div change to a different background color when your mouse hovers over it. The div should return to its original color when the mouse exits the div.
-
     let div1 = document.getElementById('div1');
 
     div1.addEventListener('mouseenter', function () {
@@ -31,36 +28,33 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     // #4. Put some text in a paragraph. Make it where when you click on the paragraph, the color of the text switches to red. Once you get that working, try to rewrite your code to make it switch to a random color each click (you don't have to show the code for just red once you get random working).
-
     let paragraph = document.getElementById('para1');
 
     function randomColor() {
-        var color = '#' + Math.floor(Math.random()*16777215).toString(16);
+        var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
         return color;
     }
 
-    paragraph.addEventListener('click', function() {
+    paragraph.addEventListener('click', function () {
         paragraph.style.color = randomColor();
     });
 
     // #5. Add a button and an empty div. When the button is clicked, add a span that contains your name to the empty div.
-
     let nameButton = document.getElementById('nameButton');
     let nameDiv = document.getElementById('nameDiv');
 
-    nameButton.addEventListener('click', function() {
+    nameButton.addEventListener('click', function () {
         let nameSpan = document.createElement('span');
         nameDiv.appendChild(nameSpan);
         nameSpan.append('Austin Gage Jones');
     });
 
     // #6. Create a button and a ul in your HTML. In JavaScript, create an array containing the names of your friends (at least 10. If you don't have that many, include your imaginary ones). When the button is clicked, add each friend's name as an li to the ul on the page.
-
     let friendButton = document.getElementById('friendButton');
     let friendList = document.getElementById('friendList');
     let friends = ['Branwin', 'Jessie', 'Ashley', 'Austin', 'Karlie', 'Cody', 'Hunter', 'Bailey', 'Matt', 'Meghan']
 
-    friendButton.addEventListener('click', function() {
+    friendButton.addEventListener('click', function () {
         for (let i = 0; i < friends.length; i++) {
             let listItem = document.createElement('li');
             let listText = document.createTextNode(friends[i]);
@@ -68,5 +62,4 @@ window.addEventListener('DOMContentLoaded', function () {
             friendList.appendChild(listItem);
         }
     });
-
 });
--- jQuery/app.js
+++ jQuery/app.js
@@ -1,52 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // #1. Make a button appear on the page when it's first loaded. The button cannot already be in your html. When the button is clicked, you should display an Alert box with any (nice) message.
-    
     $('body').append('<button class=niceButton>Say something nice!');
-    $('.niceButton').click(function() {
+    $('.niceButton').click(function () {
         alert('Hello, beautiful! You are loved!')
     })
 
-    
     // #2. Make a button and text box (you can just put them in your HTML). When the button is clicked, display an alert with the message that is typed in the text box.
-    
-    $('#alertButton').click(function() {
+    $('#alertButton').click(function () {
         alert(`${$('#alertButtonInput').val()}`);
     })
-    
+
     // #3. Create a div in HTML. Without using CSS :hover, make the div change to a different background color when your mouse hovers over it. The div should return to its original color when the mouse exits the div.
-    
-    $('#div1').mouseenter(function() {
+    $('#div1').mouseenter(function () {
         $(this).css('background-color', 'red');
-    }).mouseleave(function() {
+    }).mouseleave(function () {
         $(this).css('background-color', '');
     })
-    
-    // #4. Put some text in a paragraph. Make it where when you click on the paragraph, the color of the text switches to red. Once you get that working, try to rewrite your code to make it switch to a random color each click (you don't have to show the code for just red once you get random working).
 
+    // #4. Put some text in a paragraph. Make it where when you click on the paragraph, the color of the text switches to red. Once you get that working, try to rewrite your code to make it switch to a random color each click (you don't have to show the code for just red once you get random working)
     function randomColor() {
-        var color = '#' + Math.floor(Math.random()*16777215).toString(16);
+        var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
         return color;
     }
 
-    $('#para1').click(function() {
-        $(this).css('color', randomColor() )
+    $('#para1').click(function () {
+        $(this).css('color', randomColor())
     })
-    
+
     // #5. Add a button and an empty div. When the button is clicked, add a span that contains your name to the empty div.
-    
-    $('#nameButton').click(function() {
+    $('#nameButton').click(function () {
         $('#nameDiv').append('<span>Austin Gage Jones')
     })
-    
-    // #6. Create a button and a ul in your HTML. In JavaScript, create an array containing the names of your friends (at least 10. If you don't have that many, include your imaginary ones). When the button is clicked, add each friend's name as an li to the ul on the page.
 
+    // #6. Create a button and a ul in your HTML. In JavaScript, create an array containing the names of your friends (at least 10. If you don't have that many, include your imaginary ones). When the button is clicked, add each friend's name as an li to the ul on the page.
     let friends = ['Branwin', 'Jessie', 'Ashley', 'Austin', 'Karlie', 'Cody', 'Hunter', 'Bailey', 'Matt', 'Meghan']
 
-    $('#friendButton').click(function() {
-        $.each(friends, function(index, value) {
+    $('#friendButton').click(function () {
+        $.each(friends, function (index, value) {
             $('#friendList').append('<li>' + value + '</li')
         })
     })
-    
+
 });
